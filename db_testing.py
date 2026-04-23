782cda143eade917a14c4346da4b1a83817d1bf2
owantland <ottog.wantland@gmail.com>
2026-04-23 20:10:22 +0200

    Condensed the table generating functions into a single one and added a filtering function for taking out the unnecessary tables from the dataset.

--- db_testing.py
+++ db_testing.py
@@ -11,10 +11,10 @@ import pandas as pd
 class generateTables():
     def __init__(self, database):
         self.database = database
-        self.ocel_path, self.ob_output, self.ev_output = self.obtain_paths()
+        self.ocel_path, self.ob_output, self.ev_output, self.filtered_tbls = self.obtain_paths()
         conn = sqlite3.connect(self.ocel_path)
         self.cursor = conn.cursor()
-        tbl
+        self.tabl_nms = self.table_names()
 
     def obtain_paths(self):
         with open('files/config.yml', 'r') as file:
@@ -23,7 +23,8 @@ class generateTables():
         ocel_path = db_configs[self.database]['ocel_path']
         ob_output = db_configs[self.database]['ob_output_path']
         ev_output = db_configs[self.database]['ev_output_path']
-        return ocel_path, ob_output, ev_output
+        filtrd_tbls = db_configs[self.database]['filtered_tables']
+        return ocel_path, ob_output, ev_output, filtrd_tbls
 
     def col_names(self, table_name):
         self.cursor.execute(f"PRAGMA table_info({table_name});")
@@ -37,12 +38,16 @@ class generateTables():
         table_names = [column[0] for column in table_names]
         return table_names
 
-    def generate_events_table(self):
+    def filter_tables(self, type):
+        fltr = type + "_"
+        tbl_nms = list(filter(lambda nm: nm.startswith(fltr), self.tabl_nms))
+        for table in self.filtered_tbls:
+            tbl_nms = list(filter(lambda nm: nm != table, tbl_nms))
+        return tbl_nms
+
+    def generate_table(self, type):
         # Get a list of all object tables
-        tabl_nms = self.table_names()
-        tabl_nms = list(filter(lambda nm: nm.startswith('event_'), tabl_nms))
-        tabl_nms = list(filter(lambda nm: nm != 'event_map_type', tabl_nms))
-        tables = list(filter(lambda nm: nm != 'event_object', tabl_nms))
+        tables = self.filter_tables(type)
         cols = set()
 
         # Create a list of all columns for the union table
@@ -65,67 +70,22 @@ class generateTables():
                     qry_cols += f"NULL as '{column}',\n"
 
             query = f'''
-                        SELECT
-                            {qry_cols}
-                            event.ocel_type
-                        FROM {table}
-                        JOIN EVENT ON {table}.ocel_id = event.ocel_id
-                        ORDER BY 1
-                        LIMIT 10;
-                    '''
+                                SELECT
+                                    {qry_cols}
+                                    {type}.ocel_type
+                                FROM {table}
+                                JOIN {type} ON {table}.ocel_id = {type}.ocel_id
+                                ORDER BY 1
+                                LIMIT 10;
+                            '''
             self.cursor.execute(query)
             columns_info = self.cursor.fetchall()
             for column in columns_info:
                 ev_df.loc[len(ev_df.index)] = column
-            ev_df.to_csv(self.ev_output, sep=',', index=False)
-
-
-    def generate_objects_table(self):
-        '''
-        Given an sqlite database, generate an event table as a union of all relevant events
-        :return: .csv file with the complete table
-        '''
-        # Get a list of all object tables
-        tabl_nms = self.table_names()
-        tabl_nms = list(filter(lambda nm: nm.startswith('object_'), tabl_nms))
-        tabl_nms = list(filter(lambda nm: nm != 'object_map_type', tabl_nms))
-        tables = list(filter(lambda nm: nm != 'object_object', tabl_nms))
-        cols = set()
-
-        # Get the set of columns that the big table needs to have
-        for table in tables:
-            columns = self.col_names(table)
-            for column in columns:
-                cols.add(column)
-
-        # Check each table for which columns they have
-        col_names = list(cols)
-        col_names.append('type')
-        ob_df = pd.DataFrame(columns=col_names)
-        for table in tables:
-            qry_cols = ""
-            columns = self.col_names(table)
-            for column in cols:
-                if column in columns:
-                    qry_cols += f"{table}.'{column}',\n"
-                else:
-                    qry_cols += f"NULL as '{column}',\n"
-
-            query = f'''
-                        SELECT
-                            {qry_cols}
-                            object.ocel_type
-                        FROM {table}
-                        JOIN OBJECT ON {table}.ocel_id = OBJECT.ocel_id
-                        ORDER BY 1
-                        LIMIT 10;
-                    '''
-            self.cursor.execute(query)
-            columns_info = self.cursor.fetchall()
-            for column in columns_info:
-                ob_df.loc[len(ob_df.index)] = column
-        ob_df.to_csv(self.ob_output, sep=',', index=False)
-
+            if type == 'event':
+                ev_df.to_csv(self.ev_output, sep=',', index=False)
+            else:
+                ev_df.to_csv(self.ob_output, sep=',', index=False)
 
     def generate_event_objects_table(self):
         # Get a list of all object tables
@@ -170,7 +130,9 @@ class generateTables():
         ob_df.to_csv(self.ob_output, sep=',', index=False)
 
 # MAIN
-database = 'aoe'
+database = 'logistics'
 tbl = generateTables(database)
-tbl.generate_events_table()
-tbl.generate_objects_table()
+tbl.generate_table('event')
+tbl.generate_table('object')
+# tbl.generate_events_table()
+# tbl.generate_objects_table()
