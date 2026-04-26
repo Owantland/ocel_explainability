a525acb6e40e3748f8dfc1e9dd29931596d91a54
owantland <ottog.wantland@gmail.com>
2026-04-26 20:45:11 +0200

    Created initial fucntion for creating the OCEL from the event database.

--- db_testing.py
+++ db_testing.py
@@ -45,6 +45,107 @@ class generateTables():
             tbl_nms = list(filter(lambda nm: nm != table, tbl_nms))
         return tbl_nms
 
+    # Obtain the timestamped series of events present in the event data set
+    def event_log(self):
+        tables = self.filter_tables('event')
+        cols = set()
+
+        # Create a list of all columns for the union table
+        for table in tables:
+            columns = self.col_names(table)
+            for column in columns:
+                cols.add(column)
+
+        # Check each table for which columns they have
+        col_names = list(cols)
+        col_names.append('type')
+        ev_df = pd.DataFrame(columns=col_names)
+        for table in tables:
+            qry_cols = ""
+            columns = self.col_names(table)
+            for column in cols:
+                if column in columns:
+                    qry_cols += f"{table}.'{column}',\n"
+                else:
+                    qry_cols += f"NULL as '{column}',\n"
+
+            query = f'''
+                        SELECT DISTINCT
+                            {qry_cols}
+                            event.ocel_type
+                        FROM {table}
+                        JOIN event ON {table}.ocel_id = event.ocel_id
+                        ORDER BY 1;
+                    '''
+            print(query)
+            self.cursor.execute(query)
+            columns_info = self.cursor.fetchall()
+            for column in columns_info:
+                ev_df.loc[len(ev_df.index)] = column
+
+            ev_df.to_csv(self.ev_output, sep=',', index=False)
+
+    # Obtain the objects associated with each event in a column wise placement
+    def object_columns(self):
+        qry = "SELECT * FROM OBJECT_MAP_TYPE"
+        self.cursor.execute(qry)
+        ob_types = self.cursor.fetchall()
+
+        # Get a list of all possible objects in the database
+        ev_ob = {}
+        ev_ob['ocel_event_id'] = []
+        for column in ob_types:
+            ev_ob[column[1]] = []
+
+        # Create a list of all events
+        qry = f'''
+                SELECT DISTINCT
+                    E.ocel_id
+                FROM EVENT E
+                ORDER BY 1;
+                '''
+        self.cursor.execute(qry)
+        columns_info = self.cursor.fetchall()
+
+        for column in columns_info:
+            ev = column[0]
+            ev_ob['ocel_event_id'].append(ev)
+
+            ev_dict = {}
+            for ob in ob_types:
+                ev_dict[ob[1]] = []
+
+            qry = f'''
+                    SELECT DISTINCT
+                        EO.OCEL_EVENT_ID,
+                        O.OCEL_ID,
+                        OCEL_TYPE_MAP
+                    FROM event_object EO
+                    JOIN OBJECT O ON EO.ocel_object_id = O.ocel_id
+                    JOIN object_map_type OM ON O.OCEL_TYPE = OM.OCEL_TYPE
+                    WHERE EO.OCEL_EVENT_ID = '{ev}'
+                    '''
+            self.cursor.execute(qry)
+            columns_info = self.cursor.fetchall()
+
+            seen_events = []
+            for column in columns_info:
+                ob_id = column[1]
+                ob_type = column[2]
+
+                ev_dict[ob_type].append(ob_id)
+
+            for ob in ob_types:
+                ob_type = ob[1]
+                ev_ob[ob_type].append(ev_dict[ob_type])
+
+        evob = pd.DataFrame.from_dict(ev_ob)
+
+        # Now, we join this table with the event log timestamps to get the whole table
+
+        evob.to_csv("tst.csv", sep=',', index=False)
+
+
     def generate_table(self, type):
         # Get a list of all object tables
         tables = self.filter_tables(type)
@@ -87,12 +188,9 @@ class generateTables():
             else:
                 ev_df.to_csv(self.ob_output, sep=',', index=False)
 
-    def generate_event_objects_table(self):
-        # Get a list of all object tables
-        tabl_nms = self.table_names()
-        tabl_nms = list(filter(lambda nm: nm.startswith('object_'), tabl_nms))
-        tabl_nms = list(filter(lambda nm: nm != 'object_map_type', tabl_nms))
-        tables = list(filter(lambda nm: nm != 'object_object', tabl_nms))
+    def generate_join_tables(self):
+        # List the tables for relations
+        tables = ['object_object', 'event_object']
         cols = set()
 
         # Get the set of columns that the big table needs to have
@@ -130,9 +228,9 @@ class generateTables():
         ob_df.to_csv(self.ob_output, sep=',', index=False)
 
 # MAIN
-database = 'logistics'
+database = 'iot'
 tbl = generateTables(database)
-tbl.generate_table('event')
-tbl.generate_table('object')
-# tbl.generate_events_table()
-# tbl.generate_objects_table()
+tbl.object_columns()
+# tbl.event_log()
+# tbl.generate_table('event')
+# tbl.generate_table('object')
