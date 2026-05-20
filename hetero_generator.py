fa6b8f8dfceeb60aa45bced4d6a3a4746ee2d2a4
owantland <ottog.wantland@gmail.com>
2026-05-20 19:29:06 +0200

    Builder function now calculates the Y values for the orders as well as the appropriate mask

--- hetero_generator.py
+++ hetero_generator.py
@@ -14,8 +14,8 @@ import warnings
 warnings.filterwarnings("ignore")
 
 class HeteroGraphsGenerator():
-    def __init__(self, database, cant, all_graphs, all_timestamps, all_idx, train_sampled_timestamps,
-                 val_sampled_timestamps, test_sampled_timestamps):
+    def __init__(self, database, cant, all_graphs, all_timestamps, all_idx, all_kpis,
+                 train_sampled_timestamps, val_sampled_timestamps, test_sampled_timestamps):
         self.database = database
         self.cant = cant
         self.num_vp_obj = self.cant
@@ -31,9 +31,10 @@ class HeteroGraphsGenerator():
         self.all_graphs = all_graphs
         self.all_timestamps = all_timestamps
         self.all_idx = all_idx
+        self.all_kpis = all_kpis
 
         # Creates a variety of dictionaries of relationships between objects
-        self.pd_active_orders, self.active_orders_dict, self.kpi_dict = self.preprocessing_steps()
+        self.pd_active_orders, self.active_orders_dict = self.preprocessing_steps()
 
         # # Creates lists of items, packages and orders and their delivery times
         # self.response_generation()
@@ -61,19 +62,13 @@ class HeteroGraphsGenerator():
         pd_active_orders = pd.DataFrame(active_orders)
         pd_active_orders.sort_values(by=2, inplace=True)
 
-        # Find the times used for calculating the chosen KPIs
-        kpi_dict = {}
-        for kpi in self.kpis:
-            events = self.pd_df[self.pd_df['type'] == kpi]
-            kpi_dict[kpi] = events
-
         # Create a dictionary of delivery times
         active_orders_dict = {}
         for idx in range(1, self.num_vp_obj + 1):
             delivery_time = self.pd_df[self.pd_df['vwpnt_id'] == idx].iloc[-1, 2]
             ob_id = self.pd_df[self.pd_df['vwpnt_id'] == idx].iloc[0, 3]
             active_orders_dict[ob_id] = delivery_time
-        return pd_active_orders, active_orders_dict, kpi_dict
+        return pd_active_orders, active_orders_dict
 
 
     def builder(self, timestamp):
@@ -86,13 +81,44 @@ class HeteroGraphsGenerator():
                          in active_orders]
         active_graphs = [copy.deepcopy(graph) for graph in active_graphs]
 
-        # current_time = pd.to_datetime(timestamp)
-        # # Calculates the time difference between the selected timestamp and the final delivery time for a package
-        # # assigned to the order
-        # # print(f'Active order time: {self.active_orders_dict}')
-        # y_order = [pd.to_datetime(self.active_orders_dict[order]) - current_time for order in active_orders]
-        # y_order = [a.total_seconds() for a in y_order]
-        # mask_order = np.array(y_order) > 0
+        # Calculate Y values
+        current_time = pd.to_datetime(timestamp)
+
+        # Find a way to save the Y values for each type
+        y_vals = {}
+        for order in active_orders:
+            kpi_df = self.all_kpis[(self.all_kpis['viewpoint_id'] == order)]
+            for kpi in self.kpis.keys():
+                try:
+                    len(y_vals[kpi]) > 0
+                except KeyError:
+                    y_vals[kpi] = {}
+                kpi_df = kpi_df[kpi_df['kpi_type'] == kpi]
+                kpi_ob_types = self.kpis[kpi]
+                for ob_type in kpi_ob_types:
+                    try:
+                        len(y_vals[kpi][ob_type]) > 0
+                    except KeyError:
+                        y_vals[kpi][ob_type] = []
+
+                    kpi_ts = kpi_df[kpi_df['ob_type'] == ob_type]['timestamp'].values
+                    kpi_ts = [pd.to_datetime(ts) - current_time for ts in kpi_ts]
+                    kpi_ts = [ts.total_seconds() for ts in kpi_ts]
+                    active_graph = self.all_graphs[(self.all_timestamps <= timestamp) & (self.all_idx == order)][-1]
+                    try:
+                        ob_cnt = len(active_graph[ob_type])
+                        y = kpi_ts[:ob_cnt]
+                        y_vals[kpi][ob_type].extend(y)
+                    except KeyError:
+                        pass
+        y_masks = {}
+        for kpi in y_vals.keys():
+            y_masks[kpi] = {}
+            for ob_type in y_vals[kpi].keys():
+                y = y_vals[kpi][ob_type]
+                y_vals[kpi][ob_type] = np.array(y)
+                y_masks[kpi][ob_type] = np.array(y) > 0
+
 
     def generate_graphs(self):
         y_train = []
