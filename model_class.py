a8f2c8d6a2dee7ee716686eb04449fbc252ae1a9
owantland <ottog.wantland@gmail.com>
2026-05-24 19:22:00 +0200

    Changed the model so it automatically add each node and edge type.

--- model_class.py
+++ model_class.py
@@ -4,92 +4,58 @@ from torch_geometric.nn import HeteroConv, GATConv, Linear
 
 
 class OrderPredictionHeteroGNN_2(torch.nn.Module):
-    def __init__(self, hidden_channels_list, out_channels, num_layers, num_heads, viewpoint):
+    def __init__(self, hidden_channels_list, out_channels, num_layers, num_heads, viewpoint, tensor_dict):
         super().__init__()
 
-        # Initialize linear layers for each node type
-        self.lin_dict = torch.nn.ModuleDict({
-            'item': Linear(2, hidden_channels_list[0] * num_heads),
-            'package': Linear(1, hidden_channels_list[0] * num_heads),
-            'employee': Linear(18, hidden_channels_list[0] * num_heads),
-            'product': Linear(2, hidden_channels_list[0] * num_heads),
-            'order': Linear(1, hidden_channels_list[0] * num_heads),
-            'event': Linear(11, hidden_channels_list[0 * num_heads]),
-            'customer': Linear(15, hidden_channels_list[0 * num_heads])
-        })
-        self.viewpoint = viewpoint
-        # Define the Heterogeneous GAT layers
+        node_dict = {}
+        edges = {}
+        edges_dict = {}
         self.convs = torch.nn.ModuleList()
+
+        for key in tensor_dict:
+            if '_to_' in key:
+                edges[key] = tensor_dict[key]
+            else:
+                # Initialize linear layers for each node type
+                node_dict[key] = Linear(tensor_dict[key], hidden_channels_list[0] * num_heads)
+        self.lin_dict = torch.nn.ModuleDict(node_dict)
+
         for i in range(num_layers):
-            conv = HeteroConv({
-                ('event', 'to', 'event'): GATConv(-1, hidden_channels_list[i], heads=num_heads, add_self_loops=False),
-                ('order', 'to', 'item'): GATConv((-1, -1), hidden_channels_list[i], heads=num_heads,
-                                                 add_self_loops=False),
-                ('order', 'to', 'event'): GATConv((-1, -1), hidden_channels_list[i], heads=num_heads,
-                                                  add_self_loops=False),
-                ('item', 'to', 'event'): GATConv((-1, -1), hidden_channels_list[i], heads=num_heads,
-                                                 add_self_loops=False),
-                ('employee', 'to', 'event'): GATConv((-1, -1), hidden_channels_list[i], heads=num_heads,
-                                                     add_self_loops=False),
-                ('package', 'to', 'event'): GATConv((-1, -1), hidden_channels_list[i], heads=num_heads,
-                                                    add_self_loops=False),
-                ('package', 'to', 'item'): GATConv((-1, -1), hidden_channels_list[i], heads=num_heads,
-                                                   add_self_loops=False),
-                ('event', 'rev_to', 'package'): GATConv((-1, -1), hidden_channels_list[i], heads=num_heads,
-                                                        add_self_loops=False),
-                ('item', 'rev_to', 'order'): GATConv((-1, -1), hidden_channels_list[i], heads=num_heads,
-                                                     add_self_loops=False),
-                ('event', 'rev_to', 'order'): GATConv((-1, -1), hidden_channels_list[i], heads=num_heads,
-                                                      add_self_loops=False),
-                ('event', 'rev_to', 'item'): GATConv((-1, -1), hidden_channels_list[i], heads=num_heads,
-                                                     add_self_loops=False),
-                ('event', 'rev_to', 'employee'): GATConv((-1, -1), hidden_channels_list[i], heads=num_heads,
-                                                         add_self_loops=False),
-                ('item', 'rev_to', 'package'): GATConv((-1, -1), hidden_channels_list[i], heads=num_heads,
-                                                       add_self_loops=False),
+            for key in edges:
+                if key == 'event_to_event':
+                    name = tuple(key.split('_'))
+                    val = GATConv(-1, hidden_channels_list[i], heads=num_heads, add_self_loops=False)
+                    edges_dict[name] = val
 
-                ('product', 'to', 'event'): GATConv((-1, -1), hidden_channels_list[i], heads=num_heads,
-                                                    add_self_loops=False),
-                ('event', 'rev_to', 'product'): GATConv((-1, -1), hidden_channels_list[i], heads=num_heads,
-                                                        add_self_loops=False),
-                ('product', 'to', 'item'): GATConv((-1, -1), hidden_channels_list[i], heads=num_heads,
-                                                   add_self_loops=False),
-                ('item', 'rev_to', 'product'): GATConv((-1, -1), hidden_channels_list[i], heads=num_heads,
-                                                       add_self_loops=False),
+                else:
+                    name = tuple(key.split('_'))
+                    val = GATConv((-1, -1), hidden_channels_list[i], heads=num_heads, add_self_loops=False)
+                    edges_dict[name] = val
 
-                ('customer', 'to', 'order'): GATConv((-1, -1), hidden_channels_list[i], heads=num_heads,
-                                                     add_self_loops=False),
-                ('customer', 'to', 'event'): GATConv((-1, -1), hidden_channels_list[i], heads=num_heads,
-                                                     add_self_loops=False),
-
-                ('employee', 'to', 'package'): GATConv((-1, -1), hidden_channels_list[i], heads=num_heads,
-                                                       add_self_loops=False),
-                ('package', 'rev_to', 'employee'): GATConv((-1, -1), hidden_channels_list[i], heads=num_heads,
-                                                           add_self_loops=False),
-
-                ('order', 'rev_to', 'customers'): GATConv((-1, -1), hidden_channels_list[i], heads=num_heads,
-                                                          add_self_loops=False),
-                ('event', 'rev_to', 'customer'): GATConv((-1, -1), hidden_channels_list[i], heads=num_heads,
-                                                         add_self_loops=False)
-            }, aggr='sum')
+                    name = key.replace('_to_', '--rev_to--')
+                    name = name.split('--')
+                    name = tuple(reversed(name))
+                    val = GATConv((-1, -1), hidden_channels_list[i], heads=num_heads, add_self_loops=False)
+                    edges_dict[name] = val
+            conv = HeteroConv(edges_dict, aggr='sum')
             self.convs.append(conv)
 
         # Linear layer for final prediction specific to "order" nodes
         self.lin_out = Linear(hidden_channels_list[-1] * num_heads, out_channels)
 
-    def forward(self, batch):
-        # Apply linear transformations to the initial features of each node type
-        x_dict = {key: self.lin_dict[key](x) for key, x in batch.x_dict.items()}
-        edge_index_dict = batch.edge_index_dict
-
-        # Apply each HeteroConv layer with GATConv
-        for conv in self.convs:
-            x_dict = conv(x_dict, edge_index_dict)
-            # Apply ReLU activation after each convolution layer
-            x_dict = {key: F.relu(x) for key, x in x_dict.items()}
-
-        # Only use the embeddings for 'order' nodes for final prediction
-        order_out = self.lin_out(x_dict[self.viewpoint])
-
-        return order_out
+    # def forward(self, batch):
+    #     # Apply linear transformations to the initial features of each node type
+    #     x_dict = {key: self.lin_dict[key](x) for key, x in batch.x_dict.items()}
+    #     edge_index_dict = batch.edge_index_dict
+    #
+    #     # Apply each HeteroConv layer with GATConv
+    #     for conv in self.convs:
+    #         x_dict = conv(x_dict, edge_index_dict)
+    #         # Apply ReLU activation after each convolution layer
+    #         x_dict = {key: F.relu(x) for key, x in x_dict.items()}
+    #
+    #     # Only use the embeddings for 'order' nodes for final prediction
+    #     order_out = self.lin_out(x_dict[self.viewpoint])
+    #
+    #     return order_out
 
