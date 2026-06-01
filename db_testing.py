8b2101927805a31e0d9ac6ecf73116f6251c8a69
owantland <ottog.wantland@gmail.com>
2026-06-01 15:52:06 +0200

    State for improved 800 record trainin/testing

--- db_testing.py
+++ db_testing.py
@@ -7,7 +7,7 @@ import evaluation as e
 
 # MAIN
 cant = 800
-database = 'logistics' #'order_management'
+database = 'order_management' #'order_management'
 
 # Obtains all related nodes and arcs in the dataset and then generates the list of process executions
 p = pg.process_generation(database, cant)
@@ -16,23 +16,20 @@ p.get_ev_log(nodes)
 
 # Obtains the graph for each process execution and its associated prefixes
 tbl = tg.generateTables(database, cant)
-all_graphs, all_timestamps, all_idx, all_kpis, tensor_dict = tbl.create_graph(nodes)
+tbl.create_graph(nodes)
 
-# # # Apply the train test split to the set of process executions to obtain the relevant sets for learning set generation
-# ttb = tb.TrainTestBuilder(database, cant)
-# train_sampled_timestamps, val_sampled_timestamps, test_sampled_timestamps = ttb.timestamps_generator()
-#
+# Apply the train test split to the set of process executions to obtain the relevant sets for learning set generation
+ttb = tb.TrainTestBuilder(database, cant)
+train_sampled_timestamps, val_sampled_timestamps, test_sampled_timestamps = ttb.timestamps_generator()
+
 # # Obtains the learning set for training, testing and validation and converts it into pytorch tensors
-# hgg = hg.HeteroGraphsGenerator(database, cant, all_graphs, all_timestamps, all_idx, all_kpis, tensor_dict,
-#                                train_sampled_timestamps, val_sampled_timestamps, test_sampled_timestamps)
-# hgg.generate_graphs()
-
+hgg = hg.HeteroGraphsGenerator(database, cant, train_sampled_timestamps,
+                               val_sampled_timestamps, test_sampled_timestamps)
+hgg.generate_graphs()
 
 # #Model
-# trainer = t.Trainer(database)
-#
-# for viewpoint in ['Orders', 'Items', 'Packages']:
-#     trainer.trainer(viewpoint)
+trainer = t.Trainer(database)
+trainer.trainer()
 
 ## Evaluation of the model
 # eval = e.Evaluation()
--- training.py
+++ training.py
@@ -4,13 +4,21 @@ from model_class import OrderPredictionHeteroGNN_2
 import pandas as pd
 from torch_geometric.loader import DataLoader
 import numpy as np
-
+import yaml
+import os
 
 class Trainer():
-    def __init__(self):
+    def __init__(self, database):
+        self.database = database
+        self.get_paths()
+
         # Add this to config file
-        self.train_graphs_sg = torch.load(f'files/hetero_structures/train_graphs_sg.pt', weights_only=False)
-        self.val_graphs_sg = torch.load(f'files/hetero_structures/val_graphs_sg.pt', weights_only=False)
+        kpi_event = [x for x in self.kpis.keys()]
+        self.kpi_event = kpi_event[0]
+
+        path = f'{self.pytorch_path}{self.kpi_event}'
+        self.train_graphs_sg = torch.load(f'{path}/train_graphs_sg.pt', weights_only=False)
+        self.val_graphs_sg = torch.load(f'{path}/val_graphs_sg.pt', weights_only=False)
 
         self.criterion = torch.nn.L1Loss()
         # self.device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
@@ -18,8 +26,22 @@ class Trainer():
 
         self.model_params = pd.read_csv("files/model_parameters.csv", delimiter=',')
 
-        with open('files/tensor_dict.json') as json_file:
+        with open(f'{self.graph_output_path}tensor_dict.json') as json_file:
             self.tensor_dict = json.load(json_file)
+
+    def get_paths(self):
+        with open('files/config.yml', 'r') as file:
+            db_configs = yaml.safe_load(file)
+
+        self.output_path = db_configs[self.database]['ev_output_path']
+        self.ocel_path = db_configs[self.database]['ocel_path']
+        self.kpis = db_configs[self.database]['kpis']
+        self.pd_df = pd.read_csv(self.output_path)
+        self.viewpoint = db_configs[self.database]['viewpoint']
+        self.to_encode = db_configs[self.database]['encoding']
+        self.pytorch_path = db_configs[self.database]['pytorch_path']
+        self.graph_output_path = db_configs[self.database]['graph_output_path']
+        self.model_output_path = db_configs[self.database]['model_output_path']
 
     def training_loop(self, model, train_loader, val_loader, viewpoint, optimizer):
         model.to(self.device)  # Move the model to the GPU (or CPU)
@@ -60,48 +82,52 @@ class Trainer():
 
         return (train_loss / to_div).item(), (test_loss / to_div_2).item()
 
-    def trainer(self, kpi_ob):
+    def trainer(self):
         flag = True
-        index = ['Orders', 'Items', 'Packages'].index(kpi_ob)
-        num_layers = self.model_params.iloc[index, 1]
-        width_layers = self.model_params.iloc[index, 2]
-        heads = self.model_params.iloc[index, 3]
+        for key in self.kpis.keys():
+            ob_index = self.kpis[key]
+            for kpi_ob in self.kpis[key]:
+                # Validate model path exists
+                model_path = f"{self.model_output_path}{self.kpi_event}/{kpi_ob}"
+                if not os.path.exists(model_path):
+                    os.makedirs(model_path)
 
-        batch_size = int(self.model_params.iloc[index, 4])
-        epochs_sg = self.model_params.iloc[index, 5]
+                index = ob_index.index(kpi_ob)
+                num_layers = self.model_params.iloc[index, 1]
+                width_layers = self.model_params.iloc[index, 2]
+                heads = self.model_params.iloc[index, 3]
+                batch_size = int(self.model_params.iloc[index, 4])
+                epochs_sg = self.model_params.iloc[index, 5]
 
-        learning_rates = [0.01] * 1 + [0.0075] * 1 + [0.005] * 1 + [0.0025] * 11 + [0.001] * 10 + [0.0005] * 26
-        PATIENCE = 5
+                learning_rates = [0.01] * 1 + [0.0075] * 1 + [0.005] * 1 + [0.0025] * 11 + [0.001] * 10 + [0.0005] * 26
+                patience = 5
 
-        train_loader_sg = DataLoader(self.train_graphs_sg, shuffle=True)
-        val_loader_sg = DataLoader(self.val_graphs_sg, batch_size=len(self.val_graphs_sg))
+                train_loader_sg = DataLoader(self.train_graphs_sg, shuffle=True)
+                val_loader_sg = DataLoader(self.val_graphs_sg, batch_size=len(self.val_graphs_sg))
 
-        to_train = [i for i in range(1, 6)]
-        while flag:
-            for i in to_train:
-                model_sg = OrderPredictionHeteroGNN_2([width_layers] * num_layers, 1, num_layers,
-                                                      heads, self.tensor_dict, kpi_ob)
-                min_loss = 10e7
-                counter = 0
-
-                for epoch, lr in enumerate(learning_rates):
-                    optimizer = torch.optim.Adam(model_sg.parameters(), lr=lr)
-                    temp = self.training_loop(model_sg, train_loader_sg, val_loader_sg, kpi_ob, optimizer)
-                    print('Got temp')
-                    if temp[1] < min_loss:
-                        min_loss = temp[1]
-                        torch.save(model_sg.state_dict(), f"files/models/{kpi_ob}/GAT_sg_{i}.pth")
+                to_train = [i for i in range(1, 6)]
+                while flag:
+                    for i in to_train:
+                        model_sg = OrderPredictionHeteroGNN_2([width_layers] * num_layers, 1, num_layers,
+                                                              heads, self.tensor_dict, kpi_ob)
+                        min_loss = 10e7
                         counter = 0
-                    else:
-                        counter += 1
 
-                    if counter > PATIENCE:
-                        print('---')
-                        break
-                if epoch + 1 >= epochs_sg:
-                    to_train.remove(i)
-            if len(to_train) == 0:
-                flag = False
+                        for epoch, lr in enumerate(learning_rates):
+                            optimizer = torch.optim.Adam(model_sg.parameters(), lr=lr)
+                            temp = self.training_loop(model_sg, train_loader_sg, val_loader_sg, kpi_ob, optimizer)
+                            print('Got temp')
+                            if temp[1] < min_loss:
+                                min_loss = temp[1]
+                                torch.save(model_sg.state_dict(), f"{model_path}/GAT_sg_{i}.pth")
+                                counter = 0
+                            else:
+                                counter += 1
 
-
-
+                            if counter > patience:
+                                print('---')
+                                break
+                        if epoch + 1 >= epochs_sg:
+                            to_train.remove(i)
+                    if len(to_train) == 0:
+                        flag = False
