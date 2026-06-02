a8dfc4d0ee62388342aa355654e9d1123fe4f8a1
owantland <ottog.wantland@gmail.com>
2026-06-02 21:33:13 +0200

    Test for PayOrder at 800 registers

--- db_testing.py
+++ db_testing.py
@@ -9,14 +9,14 @@ import evaluation as e
 cant = 800
 database = 'order_management' #'order_management'
 
-# # Obtains all related nodes and arcs in the dataset and then generates the list of process executions
-# p = pg.ProcessGeneration(database, cant)
-# nodes = p.related_nodes()
-# p.get_ev_log(nodes)
-#
-# # Obtains the graph for each process execution and its associated prefixes
-# tbl = tg.GenerateTables(database, cant)
-# tbl.create_graph(nodes)
+# Obtains all related nodes and arcs in the dataset and then generates the list of process executions
+p = pg.ProcessGeneration(database, cant)
+nodes = p.related_nodes()
+p.get_ev_log(nodes)
+
+# Obtains the graph for each process execution and its associated prefixes
+tbl = tg.GenerateTables(database, cant)
+tbl.create_graph(nodes)
 
 # # Apply the train test split to the set of process executions to obtain the relevant sets for learning set generation
 # ttb = tb.TrainTestBuilder(database, cant)
@@ -26,11 +26,11 @@ database = 'order_management' #'order_management'
 # hgg = hg.HeteroGraphsGenerator(database, cant, train_sampled_timestamps,
 #                                val_sampled_timestamps, test_sampled_timestamps)
 # hgg.generate_graphs()
-#
-# # #Model
+
+# #Model
 # trainer = t.Trainer(database)
 # trainer.trainer()
 
-# Evaluation of the model
-eval = e.Evaluation(database)
-eval.evalutaion()
+# # Evaluation of the model
+# eval = e.Evaluation(database)
+# eval.evalutaion()
